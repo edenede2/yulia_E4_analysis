8bc93d9d2b06a57ba082736526547f0540a43f93
edenede2 <131803027+edenede2@users.noreply.github.com>
2023-11-18 01:47:25 +0200

    Update ppg_analyzer.py

--- ppg_analyzer.py
+++ ppg_analyzer.py
@@ -1,1 +1,62 @@
+import streamlit as st
+import pandas as pd
+import datetime
+import neurokit2 as nk
 
+# Helper Functions
+def convert_to_elapsed_time(df, initial_timestamp):
+    df['Elapsed Time'] = (df.index / df.iloc[1, 0]) + initial_timestamp
+    df['Elapsed Time'] = pd.to_datetime(df['Elapsed Time'], unit='s').dt.strftime('%H:%M:%S:%f')
+    return df
+
+def find_closest_time(event_time, ibi_data):
+    # Conversion of event_time to the same format as in ibi_data may be needed
+    closest_time = min(ibi_data['Time'], key=lambda x: abs(x - event_time))
+    return closest_time
+
+def process_bvp_signal(bvp_data, sampling_rate):
+    # Apply NeuroKit2 functions for signal processing
+    # This is a placeholder, replace with actual function calls
+    processed_signal = nk.ppg_clean(bvp_data, sampling_rate=sampling_rate)
+    return processed_signal
+
+# Streamlit App
+st.title('HRV Metrics from PPG Data')
+
+# File Uploaders
+bvp_file = st.file_uploader("Upload BVP.csv", type="csv")
+tags_file = st.file_uploader("Upload tags.csv", type="csv")
+ibi_file = st.file_uploader("Upload IBI.csv", type="csv")
+acc_file = st.file_uploader("Upload ACC.csv", type="csv")
+
+if bvp_file and tags_file and ibi_file and acc_file:
+    bvp_data = pd.read_csv(bvp_file, header=None)
+    tags_data = pd.read_csv(tags_file, header=None)
+    ibi_data = pd.read_csv(ibi_file, header=None)
+    acc_data = pd.read_csv(acc_file, header=None)
+
+    # Convert time to time from start
+    initial_timestamp = datetime.datetime.now().timestamp()  # Replace with actual initial timestamp
+    bvp_data = convert_to_elapsed_time(bvp_data, initial_timestamp)
+    ibi_data = convert_to_elapsed_time(ibi_data, initial_timestamp)
+    # Do the same for other dataframes as needed
+
+    # User interaction for event selection
+    event_name = st.selectbox('Select Event', ['Baseline', '5-Digit test', 'Exposure', 'Event1', 'Event2'])
+    event_time = st.text_input('Enter Event Time (HH:MM:SS:MS)')
+
+    # Find corresponding time in IBI data
+    closest_time = find_closest_time(event_time, ibi_data)
+
+    # Process BVP Signal
+    processed_bvp = process_bvp_signal(bvp_data, 64)  # Assuming 64Hz sampling rate
+
+    # Calculate HRV metrics
+    hrv_metrics = nk.hrv(processed_bvp, sampling_rate=64, show=True)
+    st.write(hrv_metrics)
+
+    # Visualization (if needed)
+    st.line_chart(processed_bvp)
+
+    # Download results
+    st.download_button(label="Download HRV Metrics as CSV", data=hrv_metrics.to_csv(), file_name='hrv_metrics.csv', mime='text/csv')
